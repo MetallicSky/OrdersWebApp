d86c37d06541f67d71a9b6df8dd2f3e5d6a36c68
MetallicSky <metlaski.corporatioN@gmail.com>
2024-11-11 18:00:05 +0300

    misc

---
+++ OrdersWebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrdersWebApp.Models;
+
+namespace OrdersWebApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Order> Orders { get; set; }
+    }
+}
--- OrdersWebApp/Models/Order.cs
+++ OrdersWebApp/Models/Order.cs
@@ -1,6 +1,30 @@
-﻿namespace OrdersWebApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OrdersWebApp.Models
 {
     public class Order
     {
+        [Key]
+        public int OrderId { get; set; }
+
+        [Required]
+        public string SenderCity { get; set; }
+
+        [Required]
+        public string SenderAddress { get; set; }
+
+        [Required]
+        public string RecipientCity { get; set; }
+
+        [Required]
+        public string RecipientAddress { get; set; }
+
+        [Required]
+        public decimal CargoWeight { get; set; }
+
+        [Required]
+        public DateTime PickupDate { get; set; }
+
+        public string OrderNumber { get; set; } = Guid.NewGuid().ToString().Substring(0, 8);
     }
 }
